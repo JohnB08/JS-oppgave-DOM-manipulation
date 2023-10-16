6d3bdf5ae1e772667a4e68e59d2e6b4400b56aac
JohnB08 <johnb08.kodehode@gmail.com>
2023-10-16 13:52:38 +0200

    testing different stuff JS only

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const nav = ["Home", "Learn More!", "Help", "Content List"];
 document.body.style.width = "100vw";
 document.body.style.height = "100vh";
 document.body.style.display = "grid";
-document.body.style.gridTemplateRows = "15% 1fr";
-document.body.style.gridTemplateColumns = "1fr 50% 1fr";
+document.body.style.gridTemplateRows = "20% 1fr";
+document.body.style.gridTemplateColumns = "1fr 75% 1fr";
+document.body.style.background = "blue";
 
-/* Content */
+/* Header + navbar */
 
 document.body.append(header);
 headText.textContent = "Hello!";
@@ -23,15 +24,16 @@ header.style.gridArea = "1/1/2/4";
 header.style.display = "grid";
 header.style.gridTemplateRows = "1fr 1fr";
 header.style.gridTemplateColumns = "1fr";
-header.style.background = "black";
+header.style.background = "green";
 header.style.color = "white";
+header.style.padding = "1em 0";
 headText.style.gridArea = "1/1/2/2";
 navBar.style.gridArea = "2/1/3/2";
 header.style.textAlign = "center";
 navBar.style.display = "flex";
 navBar.style.width = "100%";
 
-/* Making button */
+/* Navbar button */
 
 for (i = 0; i < nav.length; i++) {
   const btn = document.createElement("button");
@@ -40,15 +42,16 @@ for (i = 0; i < nav.length; i++) {
   btn.className = "btn";
   btn.style.width = "25%";
   btn.style.borderTop = "0.1em solid grey";
-  btn.style.borderBottom = "1em solid black";
+  btn.style.borderBottom = "0.65em solid black";
+  btn.style.borderRadius = "2em";
   btn.style.backgroundColor = "white";
-  btn.style.transition = "500ms ease-in-out";
+  btn.style.transition = "250ms ease-in-out";
   btn.addEventListener("mouseover", (event) => {
-    btn.style.borderBottom = "0.5em solid black";
-    btn.style.borderTop = "0.5em solid black";
+    btn.style.borderBottom = "0.35em solid black";
+    btn.style.borderTop = "0.3em solid black";
   });
   btn.addEventListener("mouseout", (event) => {
-    btn.style.borderBottom = "1em solid black";
+    btn.style.borderBottom = "0.65em solid black";
     btn.style.borderTop = "0.1em solid grey";
   });
 }
@@ -57,8 +60,15 @@ for (i = 0; i < nav.length; i++) {
 
 const main = document.createElement("main");
 main.className = "main";
+main.style.background = "white";
+main.style.textAlign = "center";
 main.style.gridArea = "2/2/3/3";
 document.body.append(main);
 const p = document.createElement("p");
 p.textContent = "I'm Testing stuff.";
 main.appendChild(p);
+const img = document.createElement("div");
+img.style.width = "100%";
+img.innerHTML =
+  '<img src="./img/forest_trees_autumn.jpg" alt="Autumn trees!" style="width: 100%">';
+main.appendChild(img);
